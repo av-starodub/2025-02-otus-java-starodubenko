b19c29db05dd47d3f483212a355c5464c26364cd
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-07-21 23:25:16 +0300

    Do a refactoring of SimpleJunit testing classes.

--- HW03-annotations/src/main/java/ru/otus/test/testclasses/UnexpectedExceptionInTestMethodTest.java
+++ HW03-annotations/src/main/java/ru/otus/test/testclasses/UnexpectedExceptionInTestMethodTest.java
@@ -14,14 +14,18 @@ public class UnexpectedExceptionInTestMethodTest {
     @After
     public void tearDown() {
     }
-
     @Test
-    public void throwUnexpectedException() {
-        throw new RuntimeException("Test method unexpected exception");
+    public void executeAnyWay() {
+        assertTrue(true);
     }
 
     @Test
-    public void executeAfterUnexpectedException() {
+    public void throwUnexpectedException() {
+        throw new RuntimeException("Unexpected exception in method annotated with @Test.");
+    }
+
+    @Test
+    public void executeAnyWayToo() {
         assertTrue(true);
     }
 }
