c5fb0fc4dcf42a0737e4d4c38d9b4f6d84ca5b08
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-07-21 23:25:16 +0300

    Do a refactoring of SimpleJunit testing classes.

--- HW03-annotations/src/main/java/ru/otus/test/MainSimpleJunitTest.java
+++ HW03-annotations/src/main/java/ru/otus/test/MainSimpleJunitTest.java
@@ -7,29 +7,37 @@ public class MainSimpleJunitTest {
     public static void main(String[] args) {
         new MainSimpleJunitTest().run(
                 AfterMethodWithExceptionTest.class,
-                "TestRunner should stop running after exception."
+                "TestRunner should stop executing tests after exception in method annotated with @After.",
+                "PASSED 1, FAILED 0, SKIPPED 0"
         );
         new MainSimpleJunitTest().run(
                 BeforeMethodWithExceptionTest.class,
-                "Test method should be skipped after exception."
+                "Test should be skipped after exception in method annotated with @Before.",
+                "PASSED 0, FAILED 0, SKIPPED 1"
         );
         new MainSimpleJunitTest().run(SeveralPreparatoryMethodsExecutionTest.class,
-                "All preparatory methods should be completed."
+                "Two setUp() and two tearDown() methods should be completed.",
+                "true"
         );
         new MainSimpleJunitTest().run(ExpectedExceptionInTestMethodTest.class,
-                "Test method should be passed."
+                "Test with expected exception should be passed without throw exception.",
+                "PASSED 1, FAILED 0, SKIPPED 0"
         );
         new MainSimpleJunitTest().run(UnexpectedExceptionInTestMethodTest.class,
-                "Test method should be failed and TestRunner should continue running."
+                "Test with unexpected exception should be failed and TestRunner should continue running.",
+                "PASSED 2, FAILED 1, SKIPPED 0"
         );
         new MainSimpleJunitTest().run(TestMethodsExecutionTest.class,
-                "Failed test method should throw TestException.");
+                "Failed test should throw TestException and TestRunner should continue running.",
+                "PASSED 1, FAILED 1, SKIPPED 0"
+        );
     }
 
-    private void run(Class<?> testClass, String expectations) {
+    private void run(Class<?> testClass, String testDescription, String expectedResult) {
         System.out.println("\n> " + testClass.getSimpleName());
-        System.out.println(expectations);
+        System.out.println(testDescription);
         new TestRunner(testClass).run();
+        System.out.println("Expected result: " + expectedResult);
         System.out.println("---------------------------------");
     }
 }
