e0bfe3694023a1cda0532260bbadcd6664d04aeb
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-07-22 00:13:45 +0300

    Implement the TestMethodsCash class.

--- HW03-annotations/src/main/java/ru/otus/simplejunit/methodscash/TestMethodsCash.java
+++ HW03-annotations/src/main/java/ru/otus/simplejunit/methodscash/TestMethodsCash.java
@@ -1,4 +1,26 @@
 package ru.otus.simplejunit.methodscash;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Class implements cash for all methods of test class annotated @Before, @After, @Test.
+ * HashSet for @TEST annotated methods implements undefined test execution order.
+ */
 public enum TestMethodsCash {
+    BEFORE(new ArrayList<>()),
+    AFTER(new ArrayList<>()),
+    TEST(new HashSet<>());
+
+    private final Collection<Method> methods;
+
+    TestMethodsCash(Collection<Method> methods) {
+        this.methods = methods;
+    }
+
+    public Collection<Method> getMethods() {
+        return methods;
+    }
 }
