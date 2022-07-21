65828715cee87d22b0145c3c7cf92746882d7400
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-07-22 01:24:20 +0300

    Write the logic of the TestRunner class.

--- HW03-annotations/src/main/java/ru/otus/simplejunit/runner/TestRunner.java
+++ HW03-annotations/src/main/java/ru/otus/simplejunit/runner/TestRunner.java
@@ -1,9 +1,31 @@
 package ru.otus.simplejunit.runner;
 
+import java.lang.reflect.Method;
+import java.util.Collection;
+
 public class TestRunner {
-    public TestRunner(Class<?> clazz) {
+    public TestRunner(Class<?> testClass) {
+        runClass(testClass);
     }
 
-    public void run() {
+    public void runClass(Class<?> testClass) {
+        getMethods(testClass);
+        runTests(testClass);
+        printResults();
+    }
+
+    private void getMethods(Class<?> testClass) {
+    }
+
+    private Object createTestCase(Class<?> testClass) {
+        return null;
+    }
+    private void runForEachTest(Object testCase, Collection<Method> methods) {
+    }
+
+    private void runTests(Class<?> testClass) {
+    }
+
+    private void printResults() {
     }
 }
