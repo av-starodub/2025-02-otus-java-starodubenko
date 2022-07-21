f07d6ba3a73f8959f0eb044662c686bf14e88006
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-07-22 00:23:40 +0300

    Implement the TestException class and rename package for TestMethodsCash class.

--- HW03-annotations/src/main/java/ru/otus/simplejunit/exceptions/TestException.java
+++ HW03-annotations/src/main/java/ru/otus/simplejunit/exceptions/TestException.java
@@ -1,4 +1,16 @@
 package ru.otus.simplejunit.exceptions;
 
-public class TestException {
+public class TestException extends RuntimeException {
+
+    public TestException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public TestException(String message) {
+        this(message, null);
+    }
+
+    public TestException(Exception e) {
+        this(e.getMessage(), e);
+    }
 }
