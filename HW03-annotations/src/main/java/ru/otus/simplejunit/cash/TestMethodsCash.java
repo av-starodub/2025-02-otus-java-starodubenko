e8c097d420688363c4fbbdb3d44b2f4a8a2f2b24
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-07-22 16:16:11 +0300

    Implement extracting methods from the test class.

--- HW03-annotations/src/main/java/ru/otus/simplejunit/cash/TestMethodsCash.java
+++ HW03-annotations/src/main/java/ru/otus/simplejunit/cash/TestMethodsCash.java
@@ -1,26 +1,47 @@
 package ru.otus.simplejunit.cash;
 
+import ru.otus.simplejunit.annotations.After;
+import ru.otus.simplejunit.annotations.Before;
+import ru.otus.simplejunit.annotations.Test;
+
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * Class implements cash for all methods of test class annotated @Before, @After, @Test.
  * HashSet for @TEST annotated methods implements undefined test execution order.
  */
 public enum TestMethodsCash {
-    BEFORE(new ArrayList<>()),
-    AFTER(new ArrayList<>()),
-    TEST(new HashSet<>());
+    BEFORE(Before.class, new ArrayList<>()),
+    AFTER(After.class, new ArrayList<>()),
+    TEST(Test.class, new HashSet<>());
 
     private final Collection<Method> methods;
+    private final Class<? extends Annotation> annotation;
 
-    TestMethodsCash(Collection<Method> methods) {
+    TestMethodsCash(Class<? extends Annotation> annotation, Collection<Method> methods) {
         this.methods = methods;
+        this.annotation = annotation;
     }
 
     public Collection<Method> getMethods() {
         return methods;
+    }
+
+    public static void clearCash() {
+        Arrays.stream(TestMethodsCash.values()).forEach(methodType -> methodType.methods.clear());
+    }
+
+    public static void saveMethodsInCash(Class<?> testClass) {
+        Arrays.stream(testClass.getMethods()).forEach(TestMethodsCash::addIfAnnotationPresent);
+    }
+
+    private static void addIfAnnotationPresent(Method method) {
+        Arrays.stream(TestMethodsCash.values()).forEach(cashType -> {
+            if (method.isAnnotationPresent(cashType.annotation)) {
+                cashType.methods.add(method);
+            }
+        });
     }
 }
