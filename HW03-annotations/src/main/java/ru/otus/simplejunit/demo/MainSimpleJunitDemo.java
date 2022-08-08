4c4db7cdf031b714fb0102d80100016af5c25987
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-08-08 16:28:32 +0300

    Extract the demo package from the simplejunit package.

--- HW03-annotations/src/main/java/ru/otus/simplejunit/demo/MainSimpleJunitDemo.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.simplejunit.demo;
-
-import ru.otus.simplejunit.util.TestRunner;
-
-public class MainSimpleJunitDemo {
-    public static void main(String[] args) {
-        TestRunner.run(ExampleTest.class);
-    }
-}
