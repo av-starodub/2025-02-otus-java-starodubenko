4edc34214a8aa7923634109f17665986060ff88a
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-08-15 16:48:21 +0300

    Impement classes: MainLogMethodParametersTest, Calculator, Addition.

--- HW05-aop/src/main/java/ru/otus/aop/proxy/handlers/LogMethodParametersHandler.java
+++ HW05-aop/src/main/java/ru/otus/aop/proxy/handlers/LogMethodParametersHandler.java
@@ -1,4 +1,14 @@
 package ru.otus.aop.proxy.handlers;
 
-public class LogMethodParametersHandler {
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LogMethodParametersHandler<T> implements InvocationHandler {
+    public LogMethodParametersHandler(T instance) {
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
 }
