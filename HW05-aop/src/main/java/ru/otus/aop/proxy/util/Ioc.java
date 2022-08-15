3dd738bc62061e676add5409c4f00cca654ebb8e
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-08-15 16:51:52 +0300

    Impement Ioc class.

--- HW05-aop/src/main/java/ru/otus/aop/proxy/util/Ioc.java
+++ HW05-aop/src/main/java/ru/otus/aop/proxy/util/Ioc.java
@@ -3,9 +3,10 @@ package ru.otus.aop.proxy.util;
 import ru.otus.aop.calculator.Calculator;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
 
 public class Ioc {
     public static Calculator createLoggedCalculator(InvocationHandler handler) {
-        return null;
+        return (Calculator) Proxy.newProxyInstance(Ioc.class.getClassLoader(), new Class<?>[]{Calculator.class}, handler);
     }
 }
