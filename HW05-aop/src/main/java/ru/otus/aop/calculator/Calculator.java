c7f42986d8f73dbd8e34f0f08e4ad804e0c3de50
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-08-15 16:48:21 +0300

    Impement classes: MainLogMethodParametersTest, Calculator, Addition.

--- HW05-aop/src/main/java/ru/otus/aop/calculator/Calculator.java
+++ HW05-aop/src/main/java/ru/otus/aop/calculator/Calculator.java
@@ -1,4 +1,9 @@
 package ru.otus.aop.calculator;
 
 public interface Calculator {
+    int calculate(int number);
+
+    int calculate(int number1, int number2);
+
+    String calculate(int number1, int number2, String unit);
 }
--- HW05-aop/src/main/java/ru/otus/aop/proxy/util/Ioc.java
+++ HW05-aop/src/main/java/ru/otus/aop/proxy/util/Ioc.java
@@ -1,4 +1,11 @@
 package ru.otus.aop.proxy.util;
 
+import ru.otus.aop.calculator.Calculator;
+
+import java.lang.reflect.InvocationHandler;
+
 public class Ioc {
+    public static Calculator createLoggedCalculator(InvocationHandler handler) {
+        return null;
+    }
 }
