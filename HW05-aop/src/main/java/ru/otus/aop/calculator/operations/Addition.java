4c97ed875f3284551ba77dd9103eebe18ce006ac
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-08-15 17:35:41 +0300

    Impement LogMethodParametersHandler class.

--- HW05-aop/src/main/java/ru/otus/aop/calculator/operations/Addition.java
+++ HW05-aop/src/main/java/ru/otus/aop/calculator/operations/Addition.java
@@ -1,6 +1,7 @@
 package ru.otus.aop.calculator.operations;
 
 import ru.otus.aop.calculator.Calculator;
+import ru.otus.aop.proxy.annotations.Log;
 
 public class Addition implements Calculator {
     @Override
@@ -9,11 +10,13 @@ public class Addition implements Calculator {
     }
 
     @Override
+    @Log
     public int calculate(int number1, int number2) {
         return number1 + number2;
     }
 
     @Override
+    @Log
     public String calculate(int number1, int number2, String unit) {
         return (number1 + number2) + unit;
     }
--- HW05-aop/src/main/java/ru/otus/aop/proxy/handlers/LogMethodParametersHandler.java
+++ HW05-aop/src/main/java/ru/otus/aop/proxy/handlers/LogMethodParametersHandler.java
@@ -1,14 +1,47 @@
 package ru.otus.aop.proxy.handlers;
 
+import ru.otus.aop.proxy.annotations.Log;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Objects;
 
+import static ru.otus.aop.proxy.util.AnnotatedMethodsSupplier.*;
+
+/**
+ * The class implements the output to the console of the parameters
+ * of the class methods marked with the annotation @Log at runtime.
+ *
+ * @param <T> any instance whose class implements any interface.
+ */
 public class LogMethodParametersHandler<T> implements InvocationHandler {
+    private final T instance;
+    private final HashMap<String, String> logAnnotatedMethods;
+
     public LogMethodParametersHandler(T instance) {
+        this.instance = instance;
+        logAnnotatedMethods = getAnnotatedMethods(instance.getClass(), Log.class);
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return null;
+        if (Objects.nonNull(logAnnotatedMethods.get(createKey(method)))) {
+            printLog(createMethodParametersLog(method, args));
+        }
+        return method.invoke(instance, args);
+    }
+
+    private String createMethodParametersLog(Method method, Object[] args) {
+        StringBuilder log = new StringBuilder();
+        log.append(String.format("executed method: %s, ", method.getName()));
+        for (int idx = 0; idx < args.length; idx++) {
+            log.append(String.format("param%d=%s ", idx, args[idx]));
+        }
+        return log.toString();
+    }
+
+    private void printLog(String log) {
+        System.out.println(log);
     }
 }
