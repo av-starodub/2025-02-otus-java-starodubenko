f71e0885d45351e872b2caf72b9a2aadfd404cb2
Alexandr Starodubenko <alexvladstr@gmail.com>
2022-08-15 16:48:21 +0300

    Impement classes: MainLogMethodParametersTest, Calculator, Addition.

--- HW05-aop/src/main/java/ru/otus/aop/calculator/operations/Addition.java
+++ HW05-aop/src/main/java/ru/otus/aop/calculator/operations/Addition.java
@@ -3,4 +3,18 @@ package ru.otus.aop.calculator.operations;
 import ru.otus.aop.calculator.Calculator;
 
 public class Addition implements Calculator {
+    @Override
+    public int calculate(int number) {
+        return number;
+    }
+
+    @Override
+    public int calculate(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    @Override
+    public String calculate(int number1, int number2, String unit) {
+        return (number1 + number2) + unit;
+    }
 }
