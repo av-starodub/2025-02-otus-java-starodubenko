b12e576fc8b5f11c0e878eb6f20b83bb28b530d2
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-10-27 02:59:18 +0300

    Resolve merge conflict by incorporating both suggestions

--- HW03-annotations/src/main/java/ru/otus/simplejunit/exceptions/TestException.java
+++ HW03-annotations/src/main/java/ru/otus/simplejunit/exceptions/TestException.java
@@ -1,6 +1,6 @@
 package ru.otus.simplejunit.exceptions;
 
-<<<<<<< HEAD
+
 public class TestException extends RuntimeException {
 
     public TestException(String message, Throwable e) {
@@ -14,7 +14,4 @@ public class TestException extends RuntimeException {
     public TestException(Throwable e) {
         this(e.getMessage(), e);
     }
-=======
-public class TestException {
->>>>>>> 6799d7a (Create test classes for testing the SimpleJunit.)
 }
--- HW03-annotations/src/main/java/ru/otus/simplejunit/util/Assertions.java
+++ HW03-annotations/src/main/java/ru/otus/simplejunit/util/Assertions.java
@@ -1,22 +1,15 @@
 package ru.otus.simplejunit.util;
 
-<<<<<<< HEAD
 import ru.otus.simplejunit.exceptions.AssertionError;
 
 public final class Assertions {
-=======
-public class Assertions {
->>>>>>> 6799d7a (Create test classes for testing the SimpleJunit.)
     private Assertions() {
     }
 
     public static void assertTrue(boolean expression) {
-<<<<<<< HEAD
         if (!expression) {
             throw new AssertionError("expected: true, actual: false.");
         }
-=======
->>>>>>> 6799d7a (Create test classes for testing the SimpleJunit.)
     }
 
     public static void assertThrow(Class<? extends Exception> expected, Class<? extends Exception> actual) {
