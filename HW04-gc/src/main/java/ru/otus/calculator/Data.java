6c846109a25a100a1b5ebc334efd2ec37e8db91d
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-10-27 04:11:31 +0300

    Resolve merge conflict by incorporating both suggestions

--- HW04-gc/src/main/java/ru/otus/calculator/Data.java
+++ HW04-gc/src/main/java/ru/otus/calculator/Data.java
@@ -1,8 +1,6 @@
 package ru.otus.calculator;
 
 public class Data {
-<<<<<<< HEAD
-<<<<<<< HEAD
     private final int value;
 
     public Data(int value) {
@@ -10,22 +8,6 @@ public class Data {
     }
 
     public int getValue() {
-=======
-    private final Integer value;
-=======
-    private final int value;
->>>>>>> 078add0 (Optimize the application.)
-
-    public Data(int value) {
-        this.value = value;
-    }
-
-<<<<<<< HEAD
-    public Integer getValue() {
->>>>>>> f4feb63 (Set up project.)
-=======
-    public int getValue() {
->>>>>>> 078add0 (Optimize the application.)
         return value;
     }
 }
