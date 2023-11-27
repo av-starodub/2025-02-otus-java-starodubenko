8b9d67a26239a237f9f4fe3016e9a5799ecc7ca5
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-11-27 23:34:39 +0300

    Add the 'homework' module, README.md and set up the project

---
+++ HW11-jpql/homework/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,49 @@
+package ru.otus.crm.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "client_gen", sequenceName = "client_seq",
+            initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.id, this.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
