b1d6dfdb7ec4cdf6d25eb6963ad1609c3180b954
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-05-21 23:36:50 +0300

    Implement DbMigrator class

--- HW07-jdbc_advanced/src/main/java/ru/otus/advjdbc/dbmigration/DbMigrator.java
+++ HW07-jdbc_advanced/src/main/java/ru/otus/advjdbc/dbmigration/DbMigrator.java
@@ -1,15 +1,37 @@
 package ru.otus.advjdbc.dbmigration;
 
-import ru.otus.advjdbc.DataSource;
+import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
 
 public class DbMigrator {
-    private DataSource dataSource;
+    private static final Logger LOG = LoggerFactory.getLogger(DbMigrator.class);
+
+    private final Flyway flyway;
 
     public DbMigrator(DataSource dataSource) {
-        this.dataSource = dataSource;
+        flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .load();
     }
 
     public void migrate() {
+        LOG.info("***");
+        LOG.info("data base migration start ...");
+        flyway.migrate();
+        LOG.info("data base migration finished ");
+        LOG.info("***");
 
+    }
+
+    public void deleteDataBase() {
+        LOG.info("***");
+        LOG.info("data base removing start ...");
+        flyway.clean();
+        LOG.info("data base removing finished ");
+        LOG.info("***");
     }
 }
