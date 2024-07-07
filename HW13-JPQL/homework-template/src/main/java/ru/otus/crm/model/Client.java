320df5159ea7633239e2108c53e3187fdba68945
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-07-07 19:15:56 +0300

    Implement Phone class and add Lidt<Phone> field into Client class

--- HW13-JPQL/homework-template/src/main/java/ru/otus/crm/model/Client.java
+++ HW13-JPQL/homework-template/src/main/java/ru/otus/crm/model/Client.java
@@ -4,7 +4,9 @@ package ru.otus.crm.model;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
@@ -15,6 +17,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.otus.crm.model.base.AbstractBaseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 
 @Getter
@@ -34,25 +40,33 @@ public final class Client extends AbstractBaseEntity implements Cloneable {
     @JoinColumn(name = "address_id", unique = true)
     private Address address;
 
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @NotNull
+    private List<Phone> phones;
+
     public Client(String name) {
         this(null, name);
     }
 
     public Client(Long id, String name) {
-        this(id, name, null);
+        this(id, name, null, new ArrayList<>());
     }
 
-    public Client(Long id, String name, Address address) {
+    public Client(Long id, String name, Address address, List<Phone> phones) {
         requireNonNull(name, "Parameter name must not be null");
+        requireNonNull(name, "Parameter phones must not be null");
         this.id = id;
         this.name = name;
         this.address = address;
+        this.phones = phones;
+        phones.forEach(phone -> phone.setClient(this));
     }
 
     @Override
     public Client clone() {
         var clientClone = new Client(id, name);
         clientClone.setAddress(cloneAddress());
+        clientClone.setPhones(clonePhones(clientClone));
         return clientClone;
     }
 
@@ -63,9 +77,21 @@ public final class Client extends AbstractBaseEntity implements Cloneable {
         return null;
     }
 
+    private List<Phone> clonePhones(Client clientClone) {
+        if (isNull(phones)) {
+            return new ArrayList<>();
+        }
+        return phones.stream()
+                .map(phone -> {
+                    var clonePhone = new Phone(phone.getId(), phone.getNumber());
+                    clonePhone.setClient(clientClone);
+                    return clonePhone;
+                }).toList();
+    }
+
     @Override
     public String toString() {
-        return "Client{id=%d, name='%s', %s}".formatted(id,name, address);
+        return "Client{id=%d, name='%s', %s}".formatted(id, name, address);
     }
 }
 
--- HW13-JPQL/homework-template/src/main/java/ru/otus/crm/model/Phone.java
+++ HW13-JPQL/homework-template/src/main/java/ru/otus/crm/model/Phone.java
@@ -1,2 +1,49 @@
-package ru.otus.crm.model;public class Phone {
+package ru.otus.crm.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.otus.crm.model.base.AbstractBaseEntity;
+
+import static java.util.Objects.requireNonNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public final class Phone extends AbstractBaseEntity {
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "number", nullable = false)
+    @NotNull
+    private String number;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_id", updatable = false)
+    @NotNull
+    private Client client;
+
+    public Phone(String number) {
+        this(null, number);
+    }
+
+    public Phone(Long id, String number) {
+        requireNonNull(number, "Parameter number must not be null");
+        this.id = id;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{id=%d, number=%s}".formatted(id, number);
+    }
 }
