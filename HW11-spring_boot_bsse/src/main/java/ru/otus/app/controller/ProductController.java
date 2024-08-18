717e69795a121d4beb093e13aadd826fb8cd67b2
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-08-18 05:42:51 +0300

    Implement ProductController

---
+++ HW11-spring_boot_bsse/src/main/java/ru/otus/app/controller/ProductController.java
@@ -0,0 +1,53 @@
+package ru.otus.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.otus.app.dto.ProductDto;
+import ru.otus.app.dto.ProductsDto;
+import ru.otus.app.exception.ProductNotFoundException;
+import ru.otus.app.model.Product;
+import ru.otus.app.service.ProductDaoService;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    private final ProductDaoService productDaoService;
+
+    @Autowired
+    public ProductController(ProductDaoService productDaoService) {
+        this.productDaoService = productDaoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto) {
+        var createdProduct = productDaoService.create(productDto);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable("id") Long id) {
+        var productsDto = productDaoService.getById(id);
+        return new ResponseEntity<>(productsDto, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<ProductsDto> getAllProducts() {
+        var products = productDaoService.getAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+}
