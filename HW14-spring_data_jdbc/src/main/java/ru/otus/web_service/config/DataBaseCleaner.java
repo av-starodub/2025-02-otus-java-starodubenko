ab541a07baa053ab8cab5881c8ae2131d2dc9c22
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-09-05 14:37:44 +0300

    Create profiles 'dev' and 'prod' and implement DataBaseCleaner for 'dev' profile

--- HW14-spring_data_jdbc/src/main/java/ru/otus/web_service/config/DataBaseCleaner.java
+++ HW14-spring_data_jdbc/src/main/java/ru/otus/web_service/config/DataBaseCleaner.java
@@ -5,24 +5,28 @@ import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("dev")
 @RequiredArgsConstructor
-public class FlywayRepair implements ApplicationListener<ContextClosedEvent> {
-    private static final Logger logger = LoggerFactory.getLogger(FlywayRepair.class);
+public class DataBaseCleaner implements ApplicationListener<ContextClosedEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataBaseCleaner.class);
+
     private final Flyway flyway;
 
     @Override
     public void onApplicationEvent(@NonNull ContextClosedEvent event) {
         try {
-            logger.info("Flyway repairing start...");
-            flyway.repair();
-            logger.info("Flyway repairing completed successfully.");
+            logger.info("Flyway clean start...");
+            flyway.clean();
+            logger.info("Flyway clean data base completed successfully.");
         } catch (Exception e) {
-            logger.error("Flyway repairing error", e);
+            logger.error("Flyway clean error", e);
         }
     }
 }
