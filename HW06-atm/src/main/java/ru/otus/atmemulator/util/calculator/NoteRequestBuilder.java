4b0e2135a4cbe0ce911d717c300b2d14ebcdd304
Alexandr Starodubenko <alexvladstr@gmail.com>
2025-08-29 17:21:03 +0300

    Refactor `CalculationOrderType` to `NoteDispenseStrategy` with enhanced caching for note ordering

--- HW06-atm/src/main/java/ru/otus/atmemulator/util/calculator/NoteRequestBuilder.java
+++ HW06-atm/src/main/java/ru/otus/atmemulator/util/calculator/NoteRequestBuilder.java
@@ -5,17 +5,29 @@ import java.util.Map;
 import java.util.Optional;
 import ru.otus.atmemulator.denomination.Note;
 
+/**
+ * Utility class for building note-dispensing requests based on the available notes in stock
+ * and a required monetary amount. This class is particularly useful for financial systems
+ * such as ATMs to determine how to fulfill a requested sum using available notes.
+ * <p>
+ * The logic considers a specific dispensing strategy to determine the order of note selection.
+ * It ensures that the notes issued satisfy the required sum while adhering to the constraints
+ * of available inventory. If the requested sum cannot be achieved with the given stock and strategy,
+ * an empty result is returned.
+ * <p>
+ * The class is designed to be immutable and cannot be instantiated.
+ */
 public final class NoteRequestBuilder {
 
     private NoteRequestBuilder() {}
 
     public static Optional<Map<Note, Integer>> buildNoteRequestToIssue(
             Map<Note, Integer> notesInStock, int requiredSum) {
-        return buildNoteRequestToIssue(notesInStock, requiredSum, CalculationOrderType.MINIMUM_NOTES);
+        return buildNoteRequestToIssue(notesInStock, requiredSum, NoteDispenseStrategy.MINIMUM_NOTES);
     }
 
     public static Optional<Map<Note, Integer>> buildNoteRequestToIssue(
-            Map<Note, Integer> notesInStock, int requiredSum, CalculationOrderType calcType) {
+            Map<Note, Integer> notesInStock, int requiredSum, NoteDispenseStrategy dispenseStrategy) {
         var notesRequired = new HashMap<Note, Integer>();
         var residualAmount = requiredSum;
 
@@ -23,7 +35,7 @@ public final class NoteRequestBuilder {
             return Optional.of(Map.of());
         }
 
-        for (var note : calcType.sortByOrderType(notesInStock.keySet())) {
+        for (var note : dispenseStrategy.getOrderedNotes(notesInStock.keySet())) {
             var currentNominalValue = note.getNominalValue();
             var currentNominalNotesInStock = notesInStock.getOrDefault(note, 0);
             var currentNominalNotesRequired = residualAmount / currentNominalValue;
